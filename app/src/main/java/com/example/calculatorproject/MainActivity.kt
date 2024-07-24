e479431c2e02a782d828cfce627ea90f86277e5b
Dollynt <luiz.guilherme1319@gmail.com>
2024-07-24 20:46:39 -0300

    add change text button

--- app/src/main/java/com/example/calculatorproject/MainActivity.kt
+++ app/src/main/java/com/example/calculatorproject/MainActivity.kt
@@ -1,47 +1,27 @@
 package com.example.calculatorproject
 
+import android.app.Activity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.calculatorproject.ui.theme.CalculatorProjectTheme
+import com.example.calculatorproject.databinding.ActivityMainBinding
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            CalculatorProjectTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val txtUsername: TextView = findViewById(R.id.txtLabel)
+        val btnSubmit: Button = findViewById(R.id.btnAction)
+
+        btnSubmit.setOnClickListener(){
+            txtUsername.text = getString(R.string.label_changed)
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CalculatorProjectTheme {
-        Greeting("Android")
-    }
-}
