ee2173f98b8fe2623cf96949a8b7a8a616388f5e
Dollynt <luiz.guilherme1319@gmail.com>
2024-07-25 22:31:12 -0300

    add logs and override life cycle functions

--- app/src/main/java/com/example/calculatorproject/MainActivity.kt
+++ app/src/main/java/com/example/calculatorproject/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.calculatorproject
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import com.example.calculatorproject.databinding.ActivityMainBinding
+import android.util.Log
 
 class MainActivity : ComponentActivity() {
 
@@ -16,11 +18,56 @@ class MainActivity : ComponentActivity() {
         createListeners()
     }
 
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "o método onStart() foi invocado!")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "O método onRestart() foi invocado!")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "O método onResume() foi invocado!")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "O método onPause() foi invocado!")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "O método onStop() foi invocado!")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "O método onDestroy() foi invocado!")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("editTextNumber1", binding.editTextNumber1.text.toString())
+        outState.putString("editTextNumber2", binding.editTextNumber2.text.toString())
+        outState.putString("result", binding.result.text.toString())
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        binding.editTextNumber1.setText(savedInstanceState.getString("editTextNumber1"))
+        binding.editTextNumber2.setText(savedInstanceState.getString("editTextNumber2"))
+        binding.result.text = savedInstanceState.getString("result")
+    }
+
+
     private fun createListeners(){
-        binding.sumButton.setOnClickListener{ operation(option = 0) }
-        binding.subButton.setOnClickListener{ operation(option = 1) }
-        binding.multButton.setOnClickListener{ operation(option = 2) }
-        binding.divButton.setOnClickListener{ operation(option = 3) }
+        binding.sumButton.setOnClickListener{ operation( 0) }
+        binding.subButton.setOnClickListener{ operation( 1) }
+        binding.multButton.setOnClickListener{ operation( 2) }
+        binding.divButton.setOnClickListener{ operation( 3) }
     }
 
     private fun operation(option: Int){
